99996a3bcd5adc14c751959ea415789710211ea7
Dan <jihannabilanovitasari@gmail.com>
2026-09-23 00:54:58 +0800

    Add files via upload

---
+++ Praktikum Tugas 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("|   |\n");
+    printf("| - |\n");
+    printf("\n");
+    printf("Selamat Pagi,\n");
+    printf("\n");
+    printf("Teknologi Informasi FTULM Selalu dihati\n");
+
+    return 0;
+}       
